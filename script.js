3caf6825adc544cc2d2b0b129f48da4d5925079c
olndanonato-hue <olndanonato@gmail.com>
2026-04-09 19:39:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Inicializa o mapa
-const map = L.map('map').setView([-23.561684, -46.655981], 15);
+const map = L.map('map').setView([-23.687682127099368, -46.55284781881374], 15);
 
 // Adiciona o tile layer do OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,7 +7,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Adiciona um marcador na localização da loja
-L.marker([-23.561684, -46.655981])
+L.marker([-23.687682127099368, -46.55284781881374])
   .addTo(map)
-  .bindPopup("Loja Exemplo<br>Rua das Flores, 123 - São Paulo/SP")
+  .bindPopup("anhanguera sbc <br>R. Atlântica - Jardim do Mar, São Bernardo do Campo - SP, 09750-480")
   .openPopup();
